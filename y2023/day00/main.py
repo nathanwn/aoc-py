ffc856f3334b015f49af679519a2a1a89e26df71
nathanwn <49882031+nathanwn@users.noreply.github.com>
2023-12-16 22:11:35 +1000

    add template

---
+++ y2023/day00/main.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import argparse
+import os
+from collections.abc import Callable
+
+import pytest
+
+from aoclib.util import read_file
+
+
+def part1(input: str) -> int:
+    return 0
+
+
+def part2(input: str) -> int:
+    return 0
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("part", choices=["1", "2"])
+    parser.add_argument("input_file")
+
+    args = parser.parse_args()
+    input = read_file(args.input_file)
+
+    if args.part == "1":
+        print(f"Part 1: {part1(input)}")
+    else:
+        print(f"Part 2: {part2(input)}")
+
+
+@pytest.mark.skip  # remove me
+@pytest.mark.parametrize(
+    ("solver", "file", "ans"),
+    [
+        (part1, "sample.txt", -1),
+        (part2, "sample.txt", -1),
+    ],
+)
+def test(solver: Callable[[str], int], file: str, ans: int) -> None:
+    filepath = os.path.join(os.path.dirname(__file__), file)
+    assert solver(read_file(filepath)) == ans
+
+
+if __name__ == "__main__":
+    main()
